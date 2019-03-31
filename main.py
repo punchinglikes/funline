9fbd3d6d07693291742f0cc993e38f8c41c92e63
punchinglikes <punchinglikes2000@gmail.com>
2019-03-31 01:25:58 -0400

    Add spaces

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ from sys import *
 import random
 prefix = "funline >"
 verson = "1.0.0"
+ca = 'Blank'
+
 def action():
     print(' ')
     action = input(prefix)
     print(' ')
+
     return action
-ca = 'Blank'
+
 while ca == 'Blank':    
     ca = action()
+
 while ca != 'Blank':
+
     if ca == 'test':
         print('This works!')
         ca = action()
+
     elif ca == "help":
         print("Here are all of the commands")
         print("help :: shows this message")
@@ -21,17 +27,20 @@ while ca != 'Blank':
         print("test :: make sure everything is working.")
         prefix("quit :: quit the program")
         ca = action()
+
     elif ca == "prefixchange":
         request = input("What would you like to change the prefix to?: ")
         prefix = request
         print('Prefix changed to "' + prefix + '"')
         ca = action()
+
     elif ca == "quit":
         answer = input("Are you sure you want to quit [y/n]: ")
         if answer == 'y':
             quit()
         else:
             ca = action()
+
     else:
         print('Unknown command, please try again.')
         ca = action()
