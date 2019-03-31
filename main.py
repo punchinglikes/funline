a6d8df0958b666c5305ab7955d911513a45f8698
punchinglikes <punchinglikes2000@gmail.com>
2019-03-31 00:16:19 -0400

    make README and set version

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from sys import *
 import random
 prefix = "funline >"
+verson = 1.0.0
 def action():
     action = input(prefix)
     return action
 ca = 'Blank'
-while ca == 'Blank':
+while ca == 'Blank':    
     ca = action()
 
 while ca != 'Blank':
